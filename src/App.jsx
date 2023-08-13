6310eff30b1e07af52eff7dc69829af6a3ec5a64
chucoyos <unodev@outlook.com>
2023-08-13 06:32:11 -0600

    Set transition for toggle-theme feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { CssBaseline, Drawer, Avatar } from '@mui/material'
+import { CssBaseline, Drawer, Avatar, Typography } from '@mui/material'
 import LightModeIconOutlined from '@mui/icons-material/LightModeOutlined'
 import DarkModeIconOutlined from '@mui/icons-material/DarkModeOutlined'
+import Apps from '@mui/icons-material/Apps'
 import { useState } from 'react'
-import { DarkMode } from '@mui/icons-material'
 
 function App() {
-	const [customTheme, setCustomTheme] = useState(false)
+	const [customTheme, setCustomTheme] = useState(true)
 	const theme = createTheme({
 		palette: {
 			mode: customTheme ? 'dark' : 'light',
@@ -22,58 +22,81 @@ function App() {
 		<>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
-				<Drawer variant='permanent'>
-					{' '}
-					<Box
-						width={60}
+				<Container>
+					<Drawer variant='permanent'>
+						{' '}
+						<Box
+							width={60}
+							sx={{
+								display: 'flex',
+								flexDirection: 'column',
+								alignItems: 'center',
+								justifyContent: 'center',
+								gap: 2,
+								marginTop: 2,
+							}}
+						>
+							<Box onClick={toggleTheme}>
+								{customTheme ? (
+									<Avatar
+										animation='true'
+										sx={{
+											backgroundColor: 'inherit',
+											color: 'inherit',
+											border: '1px solid #fff',
+											'&:hover': {
+												paddingBottom: '4px',
+												backgroundColor: 'grey.800',
+												rotate: '360deg',
+												transition: 'all 0.5s ease-in-out',
+											},
+										}}
+									>
+										<LightModeIconOutlined fontSize='small' />
+									</Avatar>
+								) : (
+									<Avatar
+										sx={{
+											backgroundColor: 'inherit',
+											color: 'inherit',
+											border: '.1px solid #000',
+											'&:hover': {
+												paddingBottom: '4px',
+												backgroundColor: 'grey.200',
+												rotate: '360deg',
+												transition: 'all 0.5s ease-in-out',
+											},
+										}}
+									>
+										<DarkModeIconOutlined fontSize='small' />
+									</Avatar>
+								)}
+							</Box>
+							<Box
+								sx={{
+									display: 'flex',
+									flexDirection: 'column',
+									alignItems: 'center',
+								}}
+							>
+								<Apps />
+								<Typography variant='caption'>Apps</Typography>
+							</Box>
+						</Box>
+					</Drawer>
+
+					<Container
+						maxWidth='lg'
 						sx={{
 							display: 'flex',
 							flexDirection: 'column',
-							alignItems: 'center',
 							justifyContent: 'center',
-							gap: 2,
-							marginTop: 2,
+							alignItems: 'center',
 						}}
 					>
-						<Box>
-							{customTheme ? (
-								<Avatar
-									sx={{
-										backgroundColor: 'inherit',
-										color: 'inherit',
-										border: '1px solid #fff',
-									}}
-									onClick={toggleTheme}
-								>
-									<LightModeIconOutlined fontSize='small' />
-								</Avatar>
-							) : (
-								<Avatar
-									sx={{
-										backgroundColor: 'inherit',
-										color: 'inherit',
-										border: '.1px solid #000',
-									}}
-									onClick={toggleTheme}
-								>
-									<DarkModeIconOutlined fontSize='small' />
-								</Avatar>
-							)}
-						</Box>
-						<Box>
-							<DarkMode onClick={toggleTheme} />
-						</Box>
-					</Box>
-				</Drawer>
-
-				<Container
-					maxWidth='lg'
-					sx={{
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'center',
-					}}
-				></Container>
+						<Typography variant='h1'>Hello World</Typography>
+					</Container>
+				</Container>
 			</ThemeProvider>
 		</>
 	)
