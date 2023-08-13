8a7bd6ec319d10958d109f1fad5c7b38bab4626c
chucoyos <unodev@outlook.com>
2023-08-12 23:53:09 -0600

    Add toggle-dark mode feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,59 @@
 import './App.css'
-import Button from '@mui/material/Button'
+// import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { Button, CssBaseline, Paper, Drawer } from '@mui/material'
+import { useState } from 'react'
+
+const lightTheme = createTheme({
+	palette: {
+		mode: 'light',
+	},
+})
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+})
 
 function App() {
+	const [theme, setTheme] = useState(lightTheme)
+	const toggleTheme = () => {
+		theme === lightTheme ? setTheme(darkTheme) : setTheme(lightTheme)
+	}
 	return (
 		<>
-			<h1>Memory Card</h1>
-			<Button variant='contained'>rerwer</Button>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Drawer
+					variant='permanent'
+					anchor='right'
+				></Drawer>
+
+				<Container
+					maxWidth='lg'
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+					}}
+				>
+					<Paper>
+						<Box>
+							<Button
+								variant='contained'
+								onClick={() => {
+									toggleTheme()
+								}}
+							>
+								qwewqe
+							</Button>
+						</Box>
+					</Paper>
+				</Container>
+			</ThemeProvider>
 		</>
 	)
 }
