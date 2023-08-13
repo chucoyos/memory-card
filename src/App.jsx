1b049d5ac185ec154086ef435bf6223e39703177
chucoyos <unodev@outlook.com>
2023-08-13 04:18:32 -0600

    Add outlined icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-// import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { Button, CssBaseline, Paper, Drawer } from '@mui/material'
+import { CssBaseline, Drawer, Avatar } from '@mui/material'
+import LightModeIconOutlined from '@mui/icons-material/LightModeOutlined'
+import DarkModeIconOutlined from '@mui/icons-material/DarkModeOutlined'
 import { useState } from 'react'
+import { DarkMode } from '@mui/icons-material'
 
 function App() {
 	const [customTheme, setCustomTheme] = useState(false)
@@ -20,10 +22,49 @@ function App() {
 		<>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
-				<Drawer
-					variant='permanent'
-					anchor='right'
-				></Drawer>
+				<Drawer variant='permanent'>
+					{' '}
+					<Box
+						width={60}
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							justifyContent: 'center',
+							gap: 2,
+							marginTop: 2,
+						}}
+					>
+						<Box>
+							{customTheme ? (
+								<Avatar
+									sx={{
+										backgroundColor: 'inherit',
+										color: 'inherit',
+										border: '1px solid #fff',
+									}}
+									onClick={toggleTheme}
+								>
+									<LightModeIconOutlined fontSize='small' />
+								</Avatar>
+							) : (
+								<Avatar
+									sx={{
+										backgroundColor: 'inherit',
+										color: 'inherit',
+										border: '.1px solid #000',
+									}}
+									onClick={toggleTheme}
+								>
+									<DarkModeIconOutlined fontSize='small' />
+								</Avatar>
+							)}
+						</Box>
+						<Box>
+							<DarkMode onClick={toggleTheme} />
+						</Box>
+					</Box>
+				</Drawer>
 
 				<Container
 					maxWidth='lg'
@@ -32,20 +73,7 @@ function App() {
 						flexDirection: 'column',
 						justifyContent: 'center',
 					}}
-				>
-					<Paper>
-						<Box>
-							<Button
-								variant='contained'
-								onClick={() => {
-									toggleTheme()
-								}}
-							>
-								qwewqe
-							</Button>
-						</Box>
-					</Paper>
-				</Container>
+				></Container>
 			</ThemeProvider>
 		</>
 	)
