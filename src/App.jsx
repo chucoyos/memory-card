96e15b46af8f3d5c44baf780360d782716fa93cb
chucoyos <unodev@outlook.com>
2023-08-20 00:32:24 -0600

    Style AppBar

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ import {
 	Typography,
 	Chip,
 	Paper,
+	AppBar,
+	Toolbar,
+	IconButton,
 } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
 import LightModeIconOutlined from '@mui/icons-material/LightModeOutlined'
 import DarkModeIconOutlined from '@mui/icons-material/DarkModeOutlined'
 import Apps from '@mui/icons-material/Apps'
@@ -74,6 +78,53 @@ function App() {
 		<>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
+				<Breakpoint
+					small
+					down
+				>
+					<Box marginBottom={8}>
+						<AppBar
+							sx={{
+								backgroundColor: theme.primary.main,
+								color: theme.primary.onPrimary,
+							}}
+						>
+							<Toolbar>
+								<IconButton
+									size='large'
+									edge='start'
+									color='inherit'
+									aria-label='menu'
+									sx={{ mr: 2 }}
+								>
+									<MenuIcon />
+								</IconButton>
+								{theme === lightTheme ? (
+									<IconButton
+										size='large'
+										color='inherit'
+										aria-label='dark mode'
+										sx={{ mr: 2 }}
+										onClick={toggleTheme}
+										edge='end'
+									>
+										<LightModeIconOutlined fontSize='small' />
+									</IconButton>
+								) : (
+									<IconButton
+										size='large'
+										color='inherit'
+										aria-label='dark mode'
+										sx={{ mr: 2 }}
+										onClick={toggleTheme}
+									>
+										<DarkModeIconOutlined fontSize='small' />
+									</IconButton>
+								)}
+							</Toolbar>
+						</AppBar>
+					</Box>
+				</Breakpoint>
 				<Container
 					sx={{
 						display: 'flex',
--- src/MemoryCard.jsx
+++ src/MemoryCard.jsx
@@ -1,59 +1,65 @@
 /* eslint-disable react/prop-types */
 import './App.css'
-import { Paper, Typography, Box } from '@mui/material'
+import { Paper, Typography, Box, Slide } from '@mui/material'
 
 const MemoryCard = ({ theme, image, sortCards, name, artist }) => {
 	return (
-		<Paper
-			elevation={4}
-			onClick={sortCards}
-			sx={{
-				backgroundColor: theme.container.primary,
-				color: theme.onContainer.primary,
-				display: 'flex',
-				flexDirection: 'column',
-				padding: '2px',
-				width: '140px',
-				cursor: 'pointer',
-				'&:hover': {
-					transform: 'scale(1.1)',
-					transition: 'all 0.2s ease-in-out',
-				},
-			}}
+		<Slide
+			in={true}
+			timeout={500}
+			direction='up'
 		>
-			<Box>
-				<img
-					src={image}
-					alt='pokemon card'
-					style={{
-						objectFit: 'contain',
-						width: '100%',
-					}}
-				/>
-			</Box>
-			<Box
+			<Paper
+				elevation={4}
+				onClick={sortCards}
 				sx={{
+					backgroundColor: theme.container.primary,
+					color: theme.onContainer.primary,
 					display: 'flex',
 					flexDirection: 'column',
-					justifyContent: 'start',
-					padding: '4px',
+					padding: '2px',
+					width: '140px',
+					cursor: 'pointer',
+					'&:hover': {
+						transform: 'scale(1.1)',
+						transition: 'all 0.2s ease-in-out',
+					},
 				}}
 			>
-				<Typography
-					variant='subtitle1'
-					component='h2'
-					marginRight='auto'
+				<Box>
+					<img
+						src={image}
+						alt='pokemon card'
+						style={{
+							objectFit: 'contain',
+							width: '100%',
+						}}
+					/>
+				</Box>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'start',
+						padding: '4px',
+					}}
 				>
-					{name}
-				</Typography>
-				<Typography
-					variant='body2'
-					component='p'
-				>
-					{artist}
-				</Typography>
-			</Box>
-		</Paper>
+					<Typography
+						variant='subtitle1'
+						component='h2'
+						marginRight='auto'
+					>
+						{name}
+					</Typography>
+					<Typography
+						variant='body2'
+						component='p'
+					>
+						{artist}
+					</Typography>
+				</Box>
+			</Paper>
+		</Slide>
 	)
 }
 
